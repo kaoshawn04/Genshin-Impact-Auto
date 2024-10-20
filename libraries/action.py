9da25f110ec0827396d44835064cafa0b26e9a06
kaoshawn04 <ck11200373@gl.ck.tp.edu.tw>
2024-10-20 15:41:18 +0800

    2024/10/20

--- libraries/action.py
+++ libraries/action.py
@@ -14,7 +14,20 @@ class Mouse():
     
     
     @staticmethod
-    def move_to():
+    def move_to(x: int = None, y: int = None, dx: int = None, dy:int = None):
+        if x is None or y is None:
+            if dx in None or dy is None:
+                raise ValueError("Missing arguments.")
+            
+            else:
+                pass
+            
+        else:
+            pass
+    
+    
+    @staticmethod
+    def drag_to():
         pass
     
     
@@ -30,14 +43,14 @@ class Mouse():
 
 class Keyboard():
     @staticmethod
-    def _keyboard_event(message, key):
+    def _keyboard_event(event: int, key: str):
         if key in virtual_key_map.keys():
             virtual_key = virtual_key_map[key]
             
         else:
             virtual_key = ord(key.upper())
             
-        Win32api.send_input(event=message, virtual_key=virtual_key)
+        Win32api.send_input(event=event, virtual_key=virtual_key)
 
 
     @staticmethod
@@ -47,29 +60,24 @@ class Keyboard():
 
         keys = [keys] if type(keys) != list else keys
         for key in keys:
-            Keyboard._keyboard_event(message=KEYEVENTF_KEYDOWN, key=key)
+            Keyboard._keyboard_event(event=KEYEVENTF_KEYDOWN, key=key)
             time.sleep(pause)
-            Keyboard._keyboard_event(message=KEYEVENTF_KEYUP, key=key)
+            Keyboard._keyboard_event(event=KEYEVENTF_KEYUP, key=key)
 
 
     @staticmethod
     def keydown(keys: str | list):
         keys = [keys] if type(keys) != list else keys
         for key in keys:
-            Keyboard._keyboard_event(message=KEYEVENTF_EXTENDEDKEY, key=key)
+            Keyboard._keyboard_event(event=KEYEVENTF_EXTENDEDKEY, key=key)
 
 
     @staticmethod
     def keyup(keys: str | list):
         keys = [keys] if type(keys) != list else keys
         for key in keys:
-            Keyboard._keyboard_event(message=KEYEVENTF_KEYUP, key=key)  
-            
-            
+            Keyboard._keyboard_event(event=KEYEVENTF_KEYUP, key=key) 
+    
+    
 if __name__ == "__main__":
-    if not pyuac.isUserAdmin():
-        pyuac.runAsAdmin()
-        
-    time.sleep(5)
-    Keyboard.press(" ")
     
--- libraries/windows_api/api.py
+++ libraries/windows_api/api.py
@@ -1,7 +1,9 @@
 import sys
 import ctypes
 
-sys.path.append("./libraries/windows_api/common.py")
+from typing import Optional
+
+sys.path.append("./libraries/windows_api/common")
 from common import *
 
 
@@ -36,22 +38,40 @@ class Win32api():
         
         
     @staticmethod
-    def send_input(event: int, virtual_key: int):
+    def send_input(
+        type: int,
+        event: int,
+        delta_x: int = 0,
+        delta_y: int = 0,
+        delta_wheel: int = 0,
+        virtual_key: int = 0
+    ):
         """
         Synthesize keystrokes, mouse motions and button clicks.
 
         Args:
+            type: mouse: 0, keyboard: 1
             event: keydown: 0, keyup: 2.
             virtual_key: The key in ASCII code.
         """
-        input = INPUT(
-            type=ctypes.c_ulong(INPUT_KEYBOARD),
-            ki=KEYBDINPUT(
+        input = INPUT(type=ctypes.c_ulong(type))
+        
+        if type is INPUT_MOUSE:
+            input.mi = MOUSEINPUT(
+                dx=delta_x,
+                dy=delta_y,
+                mouseData=delta_wheel,
+                daFlags=event,
+                time=0
+            )
+        
+        elif type is INPUT_KEYBOARD:
+            input.ki = KEYBDINPUT(
                 wVk=virtual_key,
                 wScan=0,
                 dwFlags=event,
-                time=0,
-                #dwExtraInfo=ctypes.pointer(ctypes.c_ulong(0))
+                time=0
             )
-        )
+        
+        
         user32.SendInput(1, ctypes.pointer(input), ctypes.sizeof(INPUT))
