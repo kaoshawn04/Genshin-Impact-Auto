b9053b454a85e96b2a6fa33271d1f76b5abf7995
kaoshawn04 <ck11200373@gl.ck.tp.edu.tw>
2024-12-16 20:16:00 +0800

    2024 / 12 / 16

--- library/auto_lyre/main.py
+++ library/auto_lyre/main.py
@@ -3,7 +3,7 @@ import sys
 import time
 
 try:
-    from library.general.action import Mouse, Keyboard
+    from library.general.action import Keyboard
     from library.windows_api.api import Windows_api
     from library.auto_lyre.midi import Midi
 
@@ -11,19 +11,19 @@ except (ImportError, ModuleNotFoundError):
     dir_path = (os.path.realpath(__file__)).rsplit("\\library", 1)[0]
     sys.path.append(dir_path)
     
-    from library.general.action import Mouse, Keyboard
+    from library.general.action import Keyboard
     from library.windows_api.api import Windows_api
     from library.auto_lyre.midi import Midi
 
 
 def play(sheet):
-    sheet.pop(0) # duration
+    sheet.pop(0) # information
     
     for element in sheet:
         press_keys, wait = element[0], element[1]
         
         #print(press_keys, wait)
-        
+         
         if len(press_keys) == 1:
             if press_keys[0] is not None:
                 Keyboard.press(press_keys[0])
--- library/auto_lyre/midi.py
+++ library/auto_lyre/midi.py
@@ -1,6 +1,10 @@
 import os
+import re
 import sys
+import lxml
 import mido
+import requests
+import requests_html
 
 try:
     from library.auto_lyre.convert import Converter
@@ -13,8 +17,7 @@ except (ImportError, ModuleNotFoundError):
 
 
 class Midi():
-    def __init__(self, filepath):
-        self.filepath = filepath
+    def __init__(self):
         self.convert = Converter().frequency_to_key
 
 
@@ -24,8 +27,8 @@ class Midi():
         }
 
 
-    def process(self):
-        midi = mido.MidiFile(self.filepath, clip=True)
+    def process(self, filepath):
+        midi = mido.MidiFile(filepath, clip=True)
         result = []
 
         for i, message in enumerate(midi):
@@ -72,6 +75,57 @@ class Midi():
         return result
     
     
-if __name__ == "__main__":
-    midi = Midi("assets/midi/test.mid")
-    print(midi.process())
+    def search_musescore(self, query):
+        url = f"https://musescore.com/sheetmusic?text={query}"
+        
+        with requests_html.HTMLSession() as session:
+            response = session.get(url=url)
+            response.html.render() 
+        
+        tree = lxml.html.fromstring(response.html.html)
+        scores = tree.xpath(
+            "/html/body/div[1]/div/section/section/main/div[2]/section/article"
+        )
+        
+        result = []
+        
+        for score in scores:
+            instrument = score.xpath("./div[1]/div[2]/div[4]/a/div")[0]
+            
+            if instrument.text != "Solo Piano":
+                continue
+             
+            title = score.xpath("./div[1]/div[2]/a/h2/text() | ./div[1]/div[2]/a/h2/span/text()")
+            info = score.xpath("./div[1]/div[2]/div[2]")[0]
+            url = score.xpath("./div[1]/div[2]/a")[0]
+            
+            result.append({
+                "title": "".join(title),
+                "duration": re.search(r"\d\d:\d\d", info.text).group(),
+                "url": url.get("href")
+            })
+            
+        return result
+            
+        
+    def download_musescore(self, score_url, filepath = None):
+        base_url = "https://msdl.nanomidi.net/musescore/midi"
+        api_key = "BZ51A86BLM"
+        
+        response = requests.get(f"{base_url}?url={score_url}&api_key={api_key}").json()
+        
+        title = response["score_title"]
+        title = re.sub(r"\W+", "_", title)
+        
+        if title.startswith("_"):
+            title = title[1:]
+        
+        download_url = response["download_url"]
+        
+        response = requests.get(download_url)
+        
+        if filepath is None:
+            filepath = f"midi/{title}.mid"
+        
+        with open(filepath, "wb") as file:
+            file.write(response.content)
